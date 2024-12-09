a9486a778cc057b920e4e0911fa795bd6691e7f7
Jazyell94 <81425680+Jazyell94@users.noreply.github.com>
2024-12-09 03:06:36 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,7 +157,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (language === "br") {
       return Promise.resolve({}); // Para o idioma 'br', não precisa carregar arquivo JSON
     }
-    return fetch(`/assets/traducoes/${language}.json`)
+    return fetch(`/traducoes/${language}.json`)
       .then((response) => response.json())
       .catch((error) => {
         console.error("Erro ao carregar o arquivo de traduções:", error);
