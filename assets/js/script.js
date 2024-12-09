2adc4e4e19226ee0e724c12fbb18fdbcc90aabe4
Jazyell94 <81425680+Jazyell94@users.noreply.github.com>
2024-12-09 03:29:49 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,7 +157,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (language === "br") {
       return Promise.resolve({}); // Para o idioma 'br', não precisa carregar arquivo JSON
     }
-    return fetch(`/catre/blob/main/assets/traducoes/${language}.json`)
+    return fetch(`https://jazyell94.github.io/catre/assets/traducoes/${language}.json`)
       .then((response) => response.json())
       .catch((error) => {
         console.error("Erro ao carregar o arquivo de traduções:", error);
